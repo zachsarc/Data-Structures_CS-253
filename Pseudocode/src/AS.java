d3b4936b179846392bf208976ef24ec287a29524
Zach Lariccia <sadboigoon@gmail.com>
2025-08-28 18:24:38 -0400

    asd

---
+++ Pseudocode/src/AS.java
@@ -0,0 +1,12 @@
+public class AS {
+    public static void main(String[] args) {
+        int p = 0;
+        int i = 1;
+        int n = 4;
+
+        while (p <= n) {
+            p = p + 1;
+            i = i + 1;
+        }
+    }
+}
