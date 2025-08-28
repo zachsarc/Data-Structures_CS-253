84230d19cf619c1b76a5bd5478e8c2515bc6f091
Zach <sadboigoon@gmail.com>
2025-08-27 21:20:15 -0400

    Initial commit

---
+++ Pseudocode/src/BubbleSort.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 1, 100, 90, 76, 45};
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
+    }
+}
