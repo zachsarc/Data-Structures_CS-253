f46a19924fe44c2af708de0aca04312cf2efde70
Zach <sadboigoon@gmail.com>
2025-09-14 15:00:24 -0400

    asd

--- LinkedLists/src/LinkedListPractice.java
+++ LinkedLists/src/LinkedListPractice.java
@@ -1,4 +1,5 @@
 import java.nio.file.Path;
+import java.util.LinkedList;
 
 public class LinkedListPractice {
     Node head;
@@ -44,20 +45,43 @@ public class LinkedListPractice {
     }
 
     int deleteTail() {
-        int value;
         if (this.head == null) {
-            return -1; // no list
-        } else {
-            Node cur = this.head;
-            Node prev = null;
-            while (cur.next != null) {
-                prev = cur;
-                cur = cur.next;
-            }
-            value = cur.data;
+            return -1;
+        }
+        // Single node
+        if (this.head.next == null) {
+            int value = this.head.data;
+            this.head = null; // list becomes empty
+            return value;
+        }
+
+        // Two or more nodes
+        Node cur = this.head;
+        Node prev = null;
+        int value = this.head.data;
+
+        while (cur.next != null) {
+            prev = cur;
+            cur = cur.next;
             prev.next = null;
         }
         return value;
+    }
+
+    int deleteLast() {
+        if (this.head == null) return  -1; // List is empty
+        if (this.head.next == null) { // Single node
+            int value = this.head.data;
+            this.head = null;
+            return value;
+        }
+        Node prev = head, cur = head.next;
+        while (cur.next != null) {
+            prev = cur;
+            cur = cur.next;
+        }
+        prev.next = null;
+        return cur.data;
     }
 
     private static class Node {
@@ -68,5 +92,27 @@ public class LinkedListPractice {
             this.data = data;
             this.next = null;
         }
+    }
+
+    public static void main(String[] args) {
+        LinkedListPractice ll = new LinkedListPractice();
+        int five = 5;
+        Node newNode = new Node(five);
+        ll.addHead(five);
+        ll.addHead(3);
+        ll.addHead(12);
+        ll.addHead(63);
+        ll.printList();
+        ll.deleteLast();
+        ll.printList();
+    }
+
+    void printList() {
+        LinkedListPractice.Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null"); // Marks the end of the list
     }
 }
