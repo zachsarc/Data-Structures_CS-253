04c982217557e8db297873c821f60013dc98b692
Zach <sadboigoon@gmail.com>
2025-09-14 13:46:07 -0400

    asd

--- LinkedLists/src/LinkedListPractice.java
+++ LinkedLists/src/LinkedListPractice.java
@@ -37,8 +37,27 @@ public class LinkedListPractice {
         if (this.head == null) {
             return -1; // the list is empty
         } else {
-
+            value = this.head.data;
+            this.head = this.head.next;
         }
+        return value;
+    }
+
+    int deleteTail() {
+        int value;
+        if (this.head == null) {
+            return -1; // no list
+        } else {
+            Node cur = this.head;
+            Node prev = null;
+            while (cur.next != null) {
+                prev = cur;
+                cur = cur.next;
+            }
+            value = cur.data;
+            prev.next = null;
+        }
+        return value;
     }
 
     private static class Node {
